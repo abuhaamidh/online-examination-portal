eea3f613bf95e9c292d82c9d0024860a11dd9ee0
Nandan Pandey <pandeynandancse@gmail.com>
2019-01-04 23:14:19 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,13 +3,10 @@ session_start();
 	
     unset($_SESSION['email']);
 	
- 
     session_unset();
     session_destroy();
-
     
-	
-	header('Location: index.php');
+    header('Location: index.php');
 
 
  ?>
